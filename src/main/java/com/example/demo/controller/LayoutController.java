6cc3a7205e7ca82241217ddcda8c95599c6c3e0a
takeover-da <https://github.com/takeover-da>
2024-09-24 11:06:04 +0900

    9.24

--- src/main/java/com/example/demo/controller/LayoutController.java
+++ src/main/java/com/example/demo/controller/LayoutController.java
@@ -8,10 +8,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/layout")
 public class LayoutController {
 
-	@GetMapping("/exLayout1")
-	public void ex1() {  // /layout/exLayout1
-		// 리턴타입이 void면 URL경로가 HTML파일의 경로가 된다
+	// 리턴타입이 void면 URL경로가 HTML파일의 경로가 된다
+	@GetMapping({"/exLayout1", "/exLayout2", "/fragment2", "/fragment3", "/basic", "/exSidebar"})
+	public void ex1() {
+		
 	}
 	
 	
 }
+
+// http://localhost:8080/layout/exLayout
+// http://localhost:8080/layout/fragment
+// http://localhost:8080/layout/basic
+// http://localhost:8080/layout/exSidebar
